8eb35767b27ad489d4f7cc81145061e3584e568d
arthurkazaryan <arthur.kazaryan@yahoo.com>
2022-10-25 21:55:05 +0300

    (Настя) Внедрен препроцессинг входных данных: - приведение в нижний регистр; - удаление знаков препинания; - перевод на английский язык.

--- model/preprocessing.py
+++ model/preprocessing.py
@@ -1,7 +1,14 @@
-def preprocess_data(company_name: str):
-    """
+from googletrans import Translator
+from settings import MAKE_TRANSLATION
+import string
 
-    Функции препроцессинга
+translator = Translator(service_urls=['translate.googleapis.com'])
 
-    """
-    return company_name
+
+def preprocess_data(company_name: str, make_translation: bool = MAKE_TRANSLATION):
+
+    company_name = company_name.translate(str.maketrans('', '', string.punctuation))
+    if make_translation:
+        company_name = translator.translate(company_name, dest='en').text
+
+    return company_name.lower()
